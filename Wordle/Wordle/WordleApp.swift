06d9c08ceae7c89c8776a1bf99ec13882c90565f
Caitlin Estrada <caitlin.estrada@unc.edu>
2024-04-21 20:48:26 -0400

    Application Setup

---
+++ Wordle/Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Caitlin Estrada on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
