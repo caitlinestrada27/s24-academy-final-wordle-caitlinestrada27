0b691b24ed21dbc7dc33d9a2e3964fdf57c9ecd5
Caitlin Estrada <caitlin.estrada@unc.edu>
2024-04-22 00:44:34 -0400

    Card Flip and Key Color Change

--- Wordle/Wordle/ViewModels/WordleDataModel.swift
+++ Wordle/Wordle/ViewModels/WordleDataModel.swift
@@ -12,6 +12,8 @@ class WordleDataModel: ObservableObject {
     @Published var incorrectAttempts = [Int](repeating: 0, count: 6)
     
     var keyColors = [String : Color]()
+    var matchedLetters = [String]()
+    var misplacedLetters = [String]()
     var selectedWord = ""
     var currentWord = ""
     var tryIndex = 0
@@ -36,6 +38,9 @@ class WordleDataModel: ObservableObject {
         selectedWord = Global.commonWords.randomElement()!
         currentWord = ""
         inPlay = true
+        tryIndex = 0
+        gameOver = false
+        print(selectedWord)
     }
     
     func populateDefaults() {
@@ -48,6 +53,8 @@ class WordleDataModel: ObservableObject {
         for char in letters {
             keyColors[String(char)] = .unused
         }
+        matchedLetters = []
+        misplacedLetters = []
     }
     
     // MARK: - Game Play
@@ -67,6 +74,7 @@ class WordleDataModel: ObservableObject {
                 print("Valid word")
                 setCurrentGuessColors()
                 tryIndex += 1
+                currentWord = ""
                 if tryIndex == 6 {
                     gameOver = true
                     inPlay = false
@@ -106,6 +114,15 @@ class WordleDataModel: ObservableObject {
             let guessLetter = guesses[tryIndex].guessLetters[index]
             if guessLetter == correctLetter {
                 guesses[tryIndex].bgColors[index] = .correct
+                if !matchedLetters.contains(guessLetter) {
+                    matchedLetters.append(guessLetter)
+                    keyColors[guessLetter] = .correct
+                }
+                if misplacedLetters.contains(guessLetter) {
+                    if let index = misplacedLetters.firstIndex(where: {$0 == guessLetter}) {
+                        misplacedLetters.remove(at: index)
+                    }
+                }
                 frequency[guessLetter]! -= 1
             }
         }
@@ -114,7 +131,27 @@ class WordleDataModel: ObservableObject {
             let guessLetter = guesses[tryIndex].guessLetters[index]
             if correctLetters.contains(guessLetter) && guesses[tryIndex].bgColors[index] != .correct && frequency[guessLetter]! > 0 {
                 guesses[tryIndex].bgColors[index] = .misplaced
+                if !misplacedLetters.contains(guessLetter) && matchedLetters.contains(guessLetter){
+                    matchedLetters.append(guessLetter)
+                    keyColors[guessLetter] = .misplaced
+                }
                 frequency[guessLetter]! -= 1
+            }
+        }
+        for index in 0...4 {
+            let guessLetter = guesses[tryIndex].guessLetters[index]
+            if keyColors[guessLetter] != .correct && keyColors[guessLetter] != .misplaced {
+                    keyColors[guessLetter] = .wrong
+            }
+        }
+
+        flipCards(for: tryIndex)
+    }
+    
+    func flipCards(for row: Int) {
+        for col in 0...4 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(col) * 0.2) {
+                self.guesses[row].cardFlipped[col].toggle()
             }
         }
     }
--- Wordle/Wordle/Views/GuessView.swift
+++ Wordle/Wordle/Views/GuessView.swift
@@ -12,12 +12,20 @@ struct GuessView: View {
     var body: some View {
         HStack(spacing: 3) {
             ForEach(0...4, id: \.self) { index in
-                Text(guess.guessLetters[index])
-                    .foregroundColor(.primary)
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                    .background(Color("systemBackground"))
-                    .font(.system(size: 35, weight: .heavy))
-                    .border(Color(.secondaryLabel))
+                FlipView(isFlipped: $guess.cardFlipped[index]) {
+                    Text(guess.guessLetters[index])
+                        .foregroundColor(.primary)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        .background(Color("systemBackground"))
+
+                } back: {
+                    Text(guess.guessLetters[index])
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        .background(guess.bgColors[index])
+                }
+                .font(.system(size: 35, weight: .heavy))
+                .border(Color(.secondaryLabel))
             }
         }
     }
